2c9e6abca2aec26667171585af3fef8b0dc0b743
deepak3003 <deepaksingh76152@gmail.com>
2025-04-06 09:08:14 +0530

    Connect the database

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,34 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+   public static Connection connection;
+   public static Connection getConnection(){
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/filehider?useSSL=false","root","Deepak3003");
+
+       } catch (ClassNotFoundException | SQLException e) {
+           throw new RuntimeException(e);
+       }
+       System.out.println("Connection ho gya saab");
+       return connection;
+   }
+   public static void closeConnection(){
+       if(connection!= null){
+           try{
+               connection.close();
+           }
+           catch (SQLException ex){
+            ex.printStackTrace();
+           }
+       }
+   }
+
+    public static void main(String[] args) {
+        MyConnection.getConnection();
+    }
+}
